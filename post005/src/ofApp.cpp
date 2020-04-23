33750993c33390195772e71b258579f26fe57e25
Sonia Batllori <sbatllori@hotmail.com>
2020-04-23 19:03:49 +0200

    add(post005 release)

--- post005/src/ofApp.cpp
+++ post005/src/ofApp.cpp
@@ -4,6 +4,10 @@
 void
 ofApp::setup()
 {
+    // Frames exporter
+    framesExporter.setEnd(400);
+    framesExporter.setActive(false);
+
     // Canvas settings
     ofBackground(0);
     ofSetFrameRate(30);
@@ -12,97 +16,32 @@ ofApp::setup()
 
     // Load the character and initialize the character path
     font.load(fontName, 800, true, true, true);
-    characterPath = font.getCharacterAsPoints(character, true, false);
-    characterPath.setFilled(false);
-    characterPath.setStrokeColor(ofColor::red);
-    characterPath.setStrokeWidth(1);
+    ofPath characterPath = font.getCharacterAsPoints(character, true, false);
+
     // Center the path in the window
     ofRectangle bbox = characterPath.getOutline()[0].getBoundingBox();
-    float x = (w - bbox.width) / 2;
+    float x = (w - bbox.width) / 2 - 25;
     float y = (h + bbox.height) / 2 - 25;
-    characterPath.translate(ofVec2f(x, y));
+    characterPath.translate(ofVec2f(-1.2 * bbox.width, y));
 
-    // Lines
-    numLines = 80;
+    // Get the polyline
+    characterPoly = characterPath.getOutline()[0];
+
+    // Define lines
+    numLines = 39;
+    wMargin = 0;
+    hMargin = 0;
+
+    float x0 = wMargin;
+    float xf = w - wMargin;
+    float dy = (h - 2 * hMargin) / (numLines - 1);
+
     lines.resize(numLines);
-
-    float hMargin = 50, vMargin = 150;
-    float dy = (h - 2 * vMargin) / (numLines - 1);
-
-    for(size_t i = 0; i < numLines; i++)
+    for(int i = 0; i < numLines; i++)
     {
-        // Define line
-        soo::Line line;
-
-        // Define path
-        line.path.setFilled(false);
-        line.path.setStrokeColor(255);
-        line.path.setStrokeWidth(1);
-
-        float x0 = hMargin;
-        float y0 = vMargin + i * dy;
-
-        float xf = w - hMargin;
-        float yf = y0;
-
-        line.path.moveTo(x0, y0);
-        line.path.lineTo(xf, yf);
-
-        // Define intersection points
-        ofPolyline polyLine = line.path.getOutline()[0].getResampledBySpacing(1);
-        for(int j = 0; j < polyLine.size() - 1; j++)
-        {
-            auto& p = polyLine[j];
-            auto& pNext = polyLine[j + 1];
-
-            bool bIn = characterPath.getOutline()[0].inside(p);
-            bool bNextIn = characterPath.getOutline()[0].inside(pNext);
-
-            if(bIn ^ bNextIn)
-                line.intersectionPoints.push_back(p);
-        }
-
-        // Add line
+        float y = hMargin + i * dy;
+        auto line = std::make_shared<soo::BezierLine>(x0, xf, y, characterPoly);
         lines[i] = line;
-    }
-
-    // Bezier lines
-    bezierLines.resize(numLines);
-    for(auto& line : lines)
-    {
-        ofPath bezierLine;
-        bezierLine.setFilled(false);
-        bezierLine.setStrokeColor(255);
-        bezierLine.setStrokeWidth(1);
-
-        auto vertices = line.path.getOutline()[0].getVertices();
-        auto v0 = vertices[0];
-        auto vf = vertices[vertices.size() - 1];
-
-        // Start bezier line
-        bezierLine.moveTo(v0);
-
-        if(line.intersectionPoints.size() > 0)
-        {
-            for(int i = 0; i < line.intersectionPoints.size() - 1; i = i + 2)
-            {
-                auto& p = line.intersectionPoints[i];
-                auto& pNext = line.intersectionPoints[i + 1];
-
-                float cp1x = p.x - 20;
-                float cp1y = p.y - 30;
-                float cp2x = pNext.x + 20;
-                float cp2y = cp1y;
-
-                // bezier line to firs intersection point, and bezier to the next one
-                bezierLine.lineTo(p);
-                bezierLine.bezierTo(cp1x, cp1y, cp2x, cp2y, pNext.x, pNext.y);
-            }
-        }
-
-        // Finish bezier line
-        bezierLine.lineTo(vf);
-        bezierLines.push_back(bezierLine);
     }
 }
 
@@ -110,119 +49,18 @@ ofApp::setup()
 void
 ofApp::update()
 {
-    //    // Bezier lines
-    //    bezierLines.clear();
-    //    bezierLines.resize(numLines);
-    //    for(auto& line : lines)
-    //    {
-    //        ofPath bezierLine;
-    //        bezierLine.setFilled(false);
-    //        bezierLine.setStrokeColor(255);
-    //        bezierLine.setStrokeWidth(1);
+    framesExporter.updateByFrames(ofGetFrameNum());
 
-    //        auto vertices = line.path.getOutline()[0].getVertices();
-    //        auto v0 = vertices[0];
-    //        auto vf = vertices[vertices.size() - 1];
+    for(auto& line : lines)
+        line->moveY(-2.f, hMargin, h - hMargin, characterPoly);
 
-    //        // Start bezier line
-    //        bezierLine.moveTo(v0);
-
-    //        if(line.intersectionPoints.size() > 0)
-    //        {
-    //            for(int i = 0; i < line.intersectionPoints.size() - 1; i = i + 2)
-    //            {
-    //                auto& p = line.intersectionPoints[i];
-    //                auto& pNext = line.intersectionPoints[i + 1];
-
-    //                float cp1x = p.x - 20 + ofRandom(-15, 15);
-    //                float cp1y = p.y - 30 + ofRandom(-15, 15);
-    //                float cp2x = pNext.x + 20 + ofRandom(-15, 15);
-    //                float cp2y = pNext.y + 20 + ofRandom(-15, 15);
-
-    //                // bezier line to firs intersection point, and bezier to the next one
-    //                bezierLine.lineTo(p);
-    //                bezierLine.bezierTo(cp1x, cp1y, cp2x, cp2y, pNext.x, pNext.y);
-    //            }
-    //        }
-
-    //        // Finish bezier line
-    //        bezierLine.lineTo(vf);
-    //        bezierLines.push_back(bezierLine);
-    //}
+    characterPoly.translate(ofVec2f(5, 0));
 }
 
 //--------------------------------------------------------------
 void
 ofApp::draw()
 {
-    ofNoFill();
-    ofSetColor(ofColor::yellow);
-
-    //    characterPath.draw();
-
-    //    for(auto& line : lines)
-    //    {
-    //        //        line.path.draw();
-
-    //        for(auto& point : line.intersectionPoints)
-    //            ofDrawCircle(point, 5);
-    //    }
-
-    for(auto& bezier : bezierLines)
-        bezier.draw();
+    for(auto& line : lines)
+        line->path.draw();
 }
-
-//--------------------------------------------------------------
-void
-ofApp::keyPressed(int key)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::keyReleased(int key)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mouseMoved(int x, int y)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mouseDragged(int x, int y, int button)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mousePressed(int x, int y, int button)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mouseReleased(int x, int y, int button)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mouseEntered(int x, int y)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::mouseExited(int x, int y)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::windowResized(int w, int h)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::gotMessage(ofMessage msg)
-{}
-
-//--------------------------------------------------------------
-void
-ofApp::dragEvent(ofDragInfo dragInfo)
-{}
--- post005/src/ofApp.h
+++ post005/src/ofApp.h
@@ -1,14 +1,112 @@
 #pragma once
 
 #include "ofMain.h"
+#include "sooFramesExporter.h"
 
+// TODO move this to soolibs
 namespace soo
 {
 
-struct Line
+class BezierLine
 {
+public:
     ofPath path;
+    float x0, xf, y;
     std::vector<ofVec2f> intersectionPoints;
+
+public:
+    BezierLine(const float _x0, const float _xf, const float _y, const ofPolyline& other)
+        : x0(_x0)
+        , xf(_xf)
+        , y(_y)
+    {
+        // Path drawing settings
+        path.setFilled(false);
+        path.setStrokeColor(255);
+        path.setStrokeWidth(2);
+
+        // Path geometry
+        update(other);
+    }
+
+    void
+    moveY(const float step, const float marginUp, const float marginDown, const ofPolyline& other)
+    {
+        // Handle extreme cases
+        // a) moving down
+        if(step > 0)
+        {
+            if(y >= marginDown)
+                y = marginUp;
+        }
+        // b) moving up
+        else
+        {
+            if(y <= marginUp)
+                y = marginDown;
+        }
+
+        // Update y
+        y += step;
+
+        // Update line
+        update(other);
+        path.setStrokeColor(255 * y / ofGetHeight());
+    }
+
+private:
+    void
+    findIntersectionPoints(const ofPolyline& other)
+    {
+        // Consider the line segment inside the other's bounding box
+        ofRectangle bbox = other.getBoundingBox();
+        ofPolyline tmpLine;
+        tmpLine.addVertex(bbox.x, y);
+        tmpLine.addVertex(bbox.x + bbox.width, y);
+        ofPolyline resampled = tmpLine.getResampledBySpacing(1);
+
+        // Get the intersection points
+        intersectionPoints.clear();
+        for(int i = 0; i < resampled.size(); i++)
+        {
+            auto& p = resampled[i];
+            auto& pNext = resampled[i + 1];
+
+            bool bIn = other.inside(p);
+            bool bNextIn = other.inside(pNext);
+
+            if(bIn ^ bNextIn)
+                intersectionPoints.push_back(p);
+        }
+    }
+
+    void
+    update(const ofPolyline& other)
+    {
+        path.clear();
+        findIntersectionPoints(other);
+
+        path.moveTo(x0, y);
+        auto intersections = intersectionPoints.size();
+        if(intersections > 0)
+        {
+            for(int i = 0; i < intersections - 1; i += 2)
+            {
+                auto& p = intersectionPoints[i];
+                auto& pNext = intersectionPoints[i + 1];
+
+                // Dynamic values depending on the distance between p and pNext
+                float d = p.distance(pNext);
+                float dx = 20;
+                float dy = d / 10;
+
+                // Line to first intersection point, and bezier line to the next one
+                path.lineTo(p);
+                path.bezierTo(p.x - dx, p.y - dy, pNext.x + dx, pNext.y - dy, pNext.x, pNext.y);
+            }
+        }
+        path.lineTo(xf, y);
+    }
 };
 
 } // namespace soo
@@ -21,17 +119,8 @@ public:
     void update();
     void draw();
 
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y);
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
+    // Frames Exporter
+    soo::FramesExporter framesExporter;
 
     // Window dimensions
     float w, h;
@@ -40,10 +129,10 @@ public:
     const uint32_t character = '5';
     const string fontName = "FreeSans.ttf";
     ofTrueTypeFont font;
-    ofPath characterPath;
+    ofPolyline characterPoly;
 
     // Lines
     size_t numLines;
-    std::vector<soo::Line> lines;
-    std::vector<ofPath> bezierLines;
+    float wMargin, hMargin;
+    std::vector<std::shared_ptr<soo::BezierLine>> lines;
 };
