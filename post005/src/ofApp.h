d57b6fea82744edfbb0e5f4836ca115e54dd7975
Sonia Batllori <sbatllori@hotmail.com>
2020-10-28 19:19:02 +0100

    refactor(post005 with new libs)

--- post005/src/ofApp.h
+++ post005/src/ofApp.h
@@ -1,135 +1,66 @@
 #pragma once
 
 #include "ofMain.h"
-#include "sooFramesExporter.h"
+#include "soo_intersection.h"
 
-// TODO move this to soolibs
-namespace soo
-{
+struct HorizontalAxisWithBezierSegments {
+  float x_start_;
+  float x_end_;
+  float y_value_;
+  ofPath path_;
 
-class BezierLine
-{
-public:
-    ofPath path;
-    float x0, xf, y;
-    std::vector<ofVec2f> intersectionPoints;
+  void DefinePath() {
+    path_.clear();
 
-public:
-    BezierLine(const float _x0, const float _xf, const float _y, const ofPolyline& other)
-        : x0(_x0)
-        , xf(_xf)
-        , y(_y)
-    {
-        // Path drawing settings
-        path.setFilled(false);
-        path.setStrokeColor(255);
-        path.setStrokeWidth(2);
+    // Horizontal path from the start of the line to the end of the line
+    path_.moveTo(x_start_, y_value_);
+    path_.lineTo(x_end_, y_value_);
+  }
 
-        // Path geometry
-        update(other);
+  void DefinePath(const vector<ofVec2f>& bezier_points) {
+    path_.clear();
+
+    // The path initiates on the starting point of the line
+    path_.moveTo(x_start_, y_value_);
+
+    // For each pair of bezier points, draw a bezier segment connecting the
+    // points, and a straight horizontal line between the bezier segments
+    for (unsigned long i{0}; i < bezier_points.size() - 1; i += 2) {
+      // Define the end and starting points of the bezier segment
+      const float bezier_x_start = bezier_points[i].x;
+      const float bezier_x_end = bezier_points[i + 1].x;
+
+      // Horizontal segment to the start of the bezier segment
+      path_.lineTo(bezier_x_start, y_value_);
+
+      // Bezier line in the bezier segment (with dynamic values for the bezier
+      // intensity depending on the length of the bezier segment)
+      float dx = 20;
+      float dy = (bezier_x_end - bezier_x_start) / 10;
+      path_.bezierTo(bezier_x_start - dx, y_value_ - dy, bezier_x_end + dx,
+                     y_value_ - dy, bezier_x_end, y_value_);
     }
 
-    void moveY(const float step, const float marginUp, const float marginDown, const ofPolyline& other)
-    {
-        // Handle extreme cases
-        // a) moving down
-        if(step > 0)
-        {
-            if(y >= marginDown)
-                y = marginUp;
-        }
-        // b) moving up
-        else
-        {
-            if(y <= marginUp)
-                y = marginDown;
-        }
-
-        // Update y
-        y += step;
-
-        // Update line
-        update(other);
-        path.setStrokeColor(255 * y / ofGetHeight());
-    }
-
-private:
-    void findIntersectionPoints(const ofPolyline& other)
-    {
-        // Consider the line segment inside the other's bounding box
-        ofRectangle bbox = other.getBoundingBox();
-        ofPolyline tmpLine;
-        tmpLine.addVertex(bbox.x, y);
-        tmpLine.addVertex(bbox.x + bbox.width, y);
-        ofPolyline resampled = tmpLine.getResampledBySpacing(1);
-
-        // Get the intersection points
-        intersectionPoints.clear();
-        for(int i = 0; i < resampled.size(); i++)
-        {
-            auto& p = resampled[i];
-            auto& pNext = resampled[i + 1];
-
-            bool bIn = other.inside(p);
-            bool bNextIn = other.inside(pNext);
-
-            if(bIn ^ bNextIn)
-                intersectionPoints.push_back(p);
-        }
-    }
-
-    void update(const ofPolyline& other)
-    {
-        path.clear();
-        findIntersectionPoints(other);
-
-        path.moveTo(x0, y);
-        auto intersections = intersectionPoints.size();
-        if(intersections > 0)
-        {
-            for(int i = 0; i < intersections - 1; i += 2)
-            {
-                auto& p = intersectionPoints[i];
-                auto& pNext = intersectionPoints[i + 1];
-
-                // Dynamic values depending on the distance between p and pNext
-                float d = p.distance(pNext);
-                float dx = 20;
-                float dy = d / 10;
-
-                // Line to first intersection point, and bezier line to the next one
-                path.lineTo(p);
-                path.bezierTo(p.x - dx, p.y - dy, pNext.x + dx, pNext.y - dy, pNext.x, pNext.y);
-            }
-        }
-        path.lineTo(xf, y);
-    }
+    // Horizontal segment to the end of the line
+    path_.lineTo(x_end_, y_value_);
+  }
 };
 
-} // namespace soo
+class ofApp : public ofBaseApp {
+ public:
+  void setup();
+  void update();
+  void draw();
 
-class ofApp : public ofBaseApp
-{
-private:
-    // Frames Exporter
-    soo::FramesExporter framesExporter;
+ private:
+  const std::string kFontName_ = "FreeSans.ttf";
+  ofTrueTypeFont font_;
 
-    // Window dimensions
-    float w, h;
+  const uint32_t kChar_ = '5';
+  ofPolyline char_contour_;
 
-    // Character
-    const uint32_t character = '5';
-    const string fontName = "FreeSans.ttf";
-    ofTrueTypeFont font;
-    ofPolyline characterPoly;
-
-    // Lines
-    size_t numLines;
-    float wMargin, hMargin;
-    std::vector<std::shared_ptr<soo::BezierLine>> lines;
-
-public:
-    void setup();
-    void update();
-    void draw();
+  const float kMovingStep_ = -2.f;
+  const float kHorizontalMargin_ = 0;
+  const float kVerticalMargin_ = 0;
+  std::vector<HorizontalAxisWithBezierSegments> lines_;
 };
