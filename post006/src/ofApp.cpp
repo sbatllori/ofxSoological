04bbc719d046d51ff0e6ae1da4f76a3fd0a0f987
Sonia Batllori <sbatllori@hotmail.com>
2020-10-30 10:50:02 +0100

    refactor(post006 with new libs)

--- post006/src/ofApp.cpp
+++ post006/src/ofApp.cpp
@@ -1,32 +1,18 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void
-ofApp::setup()
-{
-    unsigned int it = 10;
-    framesExporter.setStartAndEnd(2, 3);
-    framesExporter.setActive(false);
+void ofApp::setup() {
+  // Canvas settings
+  ofSetFrameRate(30);
+  ofSetBackgroundColor(255);
+  ofSetColor(0);
+  ofSetLineWidth(1.5);
 
-    ofSetFrameRate(30);
-    ofSetBackgroundColor(255);
-    ofSetColor(0);
-    ofSetLineWidth(1.5);
-
-    dragonCurve = std::make_shared<soo::DragonCurve>(5);
-    dragonCurve->generateRecursive(it);
+  curve_ = soo::DragonCurve(kRotationAngle_);
+  curve_.GenerateRecursive(kIterations_);
 }
+//--------------------------------------------------------------
+void ofApp::update() {}
 
 //--------------------------------------------------------------
-void
-ofApp::update()
-{
-    framesExporter.updateByFrames(ofGetFrameNum());
-}
-
-//--------------------------------------------------------------
-void
-ofApp::draw()
-{
-    dragonCurve->drawStatic();
-}
+void ofApp::draw() { curve_.draw(); }
--- post006/src/ofApp.h
+++ post006/src/ofApp.h
@@ -1,117 +1,16 @@
 #pragma once
 
+#include "fractals/DragonCurve.h"
 #include "ofMain.h"
-#include "sooFramesExporter.h"
 
-namespace soo
-{
-using Node = std::shared_ptr<ofNode>;
-using Nodes = std::vector<Node>;
+class ofApp : public ofBaseApp {
+ public:
+  void setup();
+  void update();
+  void draw();
 
-class DragonCurve
-{
-private:
-    Nodes nodes;
-    float length, angle;
-
-public:
-    ofPolyline line;
-
-public:
-    DragonCurve(const float _length = 10, const float _angle = 90)
-        : length(_length)
-        , angle(_angle)
-    {}
-
-    void generateRecursive(unsigned int it)
-    {
-        computeNextIteration();
-        if(it == 0)
-            return;
-        generateRecursive(it - 1);
-    }
-
-    void drawStatic()
-    {
-        ofPushMatrix();
-        {
-            ofRectangle bbox = line.getBoundingBox();
-
-            // Define scaling parameters
-            float margin = 50;
-            float W = ofGetWidth() - 2 * margin;
-            float H = ofGetHeight() - 2 * margin;
-            float scaleFactor;
-            if(bbox.width > bbox.height)
-                scaleFactor = W / bbox.width;
-            else
-                scaleFactor = H / bbox.height;
-
-            // Define translation parameters
-            float x = ofGetWidth() / 2 - scaleFactor * (bbox.x + bbox.width / 2);
-            float y = ofGetHeight() / 2 - scaleFactor * (bbox.y + bbox.height / 2);
-
-            ofTranslate(x, y);
-            ofScale(scaleFactor);
-
-            line.draw();
-        }
-        ofPopMatrix();
-    }
-
-private:
-    void computeNextIteration()
-    {
-        if(nodes.empty())
-        {
-            Node root = std::make_shared<ofNode>();
-            root->setPosition({0, 0, 0});
-            nodes.push_back(root);
-            line.addVertex(root->getGlobalPosition());
-
-            Node node = std::make_shared<ofNode>();
-            node->setParent(*root);
-            node->setGlobalPosition({length, 0, 0});
-            nodes.push_back(node);
-            line.addVertex(node->getGlobalPosition());
-        }
-        else
-        {
-            Nodes copyNodes;
-            Node root = std::make_shared<ofNode>();
-            root->setPosition(nodes[nodes.size() - 1]->getGlobalPosition());
-            root->setOrientation(nodes[nodes.size() - 1]->getGlobalOrientation());
-            copyNodes.push_back(root);
-
-            for(int i = nodes.size() - 2; i >= 0; i--)
-            {
-                Node node = std::make_shared<ofNode>();
-                node->setParent(*root);
-                node->setGlobalPosition(nodes[i]->getGlobalPosition());
-                node->setGlobalOrientation(nodes[i]->getGlobalOrientation());
-                copyNodes.push_back(node);
-            }
-
-            root->rotateDeg(angle, {0, 0, 1});
-
-            for(size_t i = 0; i < copyNodes.size(); i++)
-            {
-                nodes.push_back(copyNodes[i]);
-                line.addVertex(copyNodes[i]->getGlobalPosition());
-            }
-        }
-    }
-};
-} // namespace soo
-
-class ofApp : public ofBaseApp
-{
-private:
-    soo::FramesExporter framesExporter;
-    std::shared_ptr<soo::DragonCurve> dragonCurve;
-
-public:
-    void setup();
-    void update();
-    void draw();
+ private:
+  const float kRotationAngle_ = 90.f;
+  const unsigned int kIterations_ = 10;
+  soo::DragonCurve curve_;
 };
