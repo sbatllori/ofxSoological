2d7987a0f79d704244101f76e3d7a47164703465
Sonia Batllori <sbatllori@hotmail.com>
2020-11-03 17:03:53 +0100

    refactor(post012 with new libs)

--- post012/README.md
+++ post012/README.md
@@ -1,6 +1,7 @@
 # Post 012: Image Color Emission
 
-Loading a colored image and a binary mask, initialize some particles in the white part of the mask, asign the image color to them, and draw their movement trace.
+Loading a colored image and a binary mask, initialize some particles in the white part of the mask,
+assign the image color to them, and draw their movement stroke.
 
 ### Input
 <img src="doc/background.jpg" width="48%"> <img src="doc/mask.jpg" width="48%">
--- post012/src/ofApp.h
+++ post012/src/ofApp.h
@@ -1,66 +1,35 @@
 #pragma once
 
 #include "ofMain.h"
-#include "sooFramesExporter.h"
-#include "sooParticle.h"
 
-namespace soo
-{
-struct Properties
-{
-    ofColor color;
-    float radius;
-    float speed;
+struct Particle {
+  ofVec2f center_;
+  float radius_;
+  ofVec2f direction_;
+  float speed_;
+  ofColor color_;
+
+  void draw() const {
+    ofNoFill();
+    ofSetColor(color_, 10);
+    ofDrawCircle(center_, radius_);
+  }
 };
-} // namespace soo
 
-using Particle = soo::Particle<soo::Properties>;
+class ofApp : public ofBaseApp {
+ public:
+  void setup();
+  void update();
+  void draw();
+  void keyPressed(int key);
 
-class ofApp : public ofBaseApp
-{
-public:
-    // Images
-    string imagePath = "012_background.jpg", maskPath = "012_mask.jpg";
-    ofImage background, mask;
+ private:
+  const std::string kImagePath_ = "012_background.jpg";
+  ofImage bg_image_;
 
-    // Particles
-    ofRectangle maskBbox;
-    unsigned long numParticles;
-    std::vector<Particle> particles;
+  const std::string kMaskPath_ = "012_mask.jpg";
+  ofImage mask_;
+  ofRectangle mask_bbox_;
 
-    // Frames Exporter
-    soo::FramesExporter framesExporter;
-
-public:
-    void setup();
-    void update();
-    void draw();
-
-    void keyPressed(int key);
-    void keyReleased(int key);
-    void mouseMoved(int x, int y);
-    void mouseDragged(int x, int y, int button);
-    void mousePressed(int x, int y, int button);
-    void mouseReleased(int x, int y, int button);
-    void mouseEntered(int x, int y);
-    void mouseExited(int x, int y);
-    void windowResized(int w, int h);
-    void dragEvent(ofDragInfo dragInfo);
-    void gotMessage(ofMessage msg);
-
-private:
-    /**
-     * @brief Initializes a new particle and adds it to the list of particles.
-     *
-     * The particles are initialized inside the loaded mask, with the pixel color of the loaded background image in
-     * that initial position, and with a vertical up direction with a bit of noise so that they move to the top of
-     * the screen.
-     */
-    void addParticle();
-
-    /**
-     * @brief Updates the position of a given particle.
-     * @param particle Particle& particle to update
-     */
-    void updateParticle(Particle& particle);
+  std::vector<Particle> particles_;
 };
