17f469d498492a5faa36bff254eaf76d74c56e7f
Sonia Batllori <sbatllori@hotmail.com>
2020-11-30 15:32:47 +0100

    add(define mandala geometry)

--- post016/src/ofApp.cpp
+++ post016/src/ofApp.cpp
@@ -9,8 +9,22 @@ void ofApp::setup() {
 
   {
     Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, .55f);
-    layer.spirograph_.AddNode({415, 0, 0}, 5.f, true);
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({470, 0, 0}, 240.f, true);
+    layer.spirograph_.AddNode({20, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 0.55f);
+    layer.spirograph_.AddNode({400, 0, 0}, 5.f, true);
     layer.spirograph_.AddNode({50, 0, 0}, 0, true);
     layers_.push_back(layer);
 
@@ -21,31 +35,186 @@ void ofApp::setup() {
                  layer.spirograph_.brush_position());
     };
   }
-
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
-    layer.spirograph_.AddNode({280, 0, 0}, 4.f, true);
-    layer.spirograph_.AddNode({100, 0, 0}, 0, true);
+    layer.spirograph_.AddNode({295, 0, 0}, 4.f, true);
+    layer.spirograph_.AddNode({70, 0, 0}, 0, true);
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
       ofSetColor(80);
-      ofSetLineWidth(8);
-      ofDrawLine(layer.previous_brush_position_,
-                 layer.spirograph_.brush_position());
-
-      ofSetColor(255);
-      ofSetLineWidth(4);
+      ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
+    layer.spirograph_.AddNode({285, 0, 0}, 4.f, true);
+    layer.spirograph_.AddNode({60, 0, 0}, 0, true);
+    layers_.push_back(layer);
 
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
+    layer.spirograph_.AddNode({290, 0, 0}, 4.f, true);
+    layer.spirograph_.AddNode({65, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 360.f / 5.f);
+    layer.spirograph_.AddNode({300, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofPushMatrix();
+      ofRotateZDeg(18.f);
+      {
+        ofSetLineWidth(2);
+
+        ofSetColor(255);
+        ofFill();
+        ofDrawCircle(layer.spirograph_.brush_position(), 20.f);
+
+        ofSetColor(80);
+        ofNoFill();
+        ofDrawCircle(layer.spirograph_.brush_position(), 20.f);
+
+        ofFill();
+        ofDrawCircle(layer.spirograph_.brush_position(), 10.f);
+      }
+      ofPopMatrix();
+    };
+  }
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
-    layer.spirograph_.AddNode({210, 0, 0}, 10.f, true);
+    layer.spirograph_.AddNode({220, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
+    layer.spirograph_.AddNode({190, 0, 0}, 5.f, true);
+    layer.spirograph_.AddNode({20, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({130, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({140, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofPushMatrix();
+      ofRotateZDeg(360.f / 40.f);
+      {
+        ofSetColor(80);
+        ofSetLineWidth(2);
+        ofDrawLine(layer.previous_brush_position_,
+                   layer.spirograph_.brush_position());
+      }
+      ofPopMatrix();
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({150, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({160, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofPushMatrix();
+      ofRotateZDeg(360.f / 40.f);
+      {
+        ofSetColor(80);
+        ofSetLineWidth(2);
+        ofDrawLine(layer.previous_brush_position_,
+                   layer.spirograph_.brush_position());
+      }
+      ofPopMatrix();
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.1f);
+    layer.spirograph_.AddNode({75, 0, 0}, 5.f, true);
+    layer.spirograph_.AddNode({50, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(80);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({20, 0, 0}, 5.f, true);
     layer.spirograph_.AddNode({10, 0, 0}, 0, true);
     layers_.push_back(layer);
 
@@ -56,50 +225,14 @@ void ofApp::setup() {
                  layer.spirograph_.brush_position());
     };
   }
-
   {
     Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, 20.f);
-    layer.spirograph_.AddNode({150, 0, 0}, 1.f, true);
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
       ofSetColor(80);
-      ofSetLineWidth(2);
-      ofNoFill();
-
-      const float radius = layer.previous_brush_position_.distance(
-                               layer.spirograph_.brush_position()) /
-                           8.f;
-      ofDrawCircle(layer.previous_brush_position_, radius);
-      ofDrawCircle(layer.spirograph_.brush_position(), radius);
-    };
-  }
-
-  {
-    Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
-    layer.spirograph_.AddNode({150, 0, 0}, 1.f, true);
-    layers_.push_back(layer);
-
-    layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(0, 100);
-      ofSetLineWidth(2);
-      ofDrawLine(layer.previous_brush_position_,
-                 layer.spirograph_.brush_position());
-    };
-  }
-
-  {
-    Layer layer;
-    layer.spirograph_.nodes_mutable().clear();
-    layer.spirograph_.AddNode({0, 0, 0}, 1.1f);
-    layer.spirograph_.AddNode({80, 0, 0}, 5.f, true);
-    layer.spirograph_.AddNode({50, 0, 0}, 0, true);
-    layers_.push_back(layer);
-
-    layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(0, 100);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
@@ -141,6 +274,9 @@ void ofApp::draw() {
 void ofApp::keyPressed(int key) {
   if (key == 's') {
     soo::SaveFrame();
+  }
+  if (key == 'r') {
+    layers_.clear();
   }
 }
 
