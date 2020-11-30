31aa10ebb0c2b08302d81fea8a2077848f2e3100
Sonia Batllori <sbatllori@hotmail.com>
2020-11-30 15:53:12 +0100

    add(tune stroke widths)

--- post016/src/ofApp.cpp
+++ post016/src/ofApp.cpp
@@ -7,6 +7,10 @@ void ofApp::setup() {
   ofSetFrameRate(3000);
   ofBackground(255);
 
+  // The following layers define the spirographs that compose the mandala from
+  // the exterior to the interior.
+
+  // Triangles pointing out
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
@@ -15,12 +19,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
+      ofSetColor(0);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Outer spiral
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 0.55f);
@@ -29,12 +35,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
+      ofSetColor(0);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // External large flower layer
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
@@ -43,12 +51,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
+      ofSetColor(0);
+      ofSetLineWidth(3);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Middle large flower layer
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
@@ -57,12 +67,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
+      ofSetColor(0);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Internal large flower layer
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
@@ -71,16 +83,18 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
+      ofSetColor(0);
+      ofSetLineWidth(1);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Circles
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 360.f / 5.f);
-    layer.spirograph_.AddNode({300, 0, 0}, 0, true);
+    layer.spirograph_.AddNode({295, 0, 0}, 0, true);
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
@@ -93,7 +107,7 @@ void ofApp::setup() {
         ofFill();
         ofDrawCircle(layer.spirograph_.brush_position(), 20.f);
 
-        ofSetColor(80);
+        ofSetColor(0);
         ofNoFill();
         ofDrawCircle(layer.spirograph_.brush_position(), 20.f);
 
@@ -103,6 +117,8 @@ void ofApp::setup() {
       ofPopMatrix();
     };
   }
+
+  // External layer of the center flower
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
@@ -111,12 +127,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
+      ofSetColor(0);
+      ofSetLineWidth(4);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Middle layer of the center flower
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.2f);
@@ -125,59 +143,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
+      ofSetColor(0);
+      ofSetLineWidth(1);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
-  {
-    Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
-    layer.spirograph_.AddNode({130, 0, 0}, 20.f, true);
-    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
-    layers_.push_back(layer);
 
-    layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
-      ofDrawLine(layer.previous_brush_position_,
-                 layer.spirograph_.brush_position());
-    };
-  }
-  {
-    Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
-    layer.spirograph_.AddNode({140, 0, 0}, 20.f, true);
-    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
-    layers_.push_back(layer);
-
-    layers_.back().draw_ = [](const Layer& layer) {
-      ofPushMatrix();
-      ofRotateZDeg(360.f / 40.f);
-      {
-        ofSetColor(80);
-        ofSetLineWidth(2);
-        ofDrawLine(layer.previous_brush_position_,
-                   layer.spirograph_.brush_position());
-      }
-      ofPopMatrix();
-    };
-  }
-  {
-    Layer layer;
-    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
-    layer.spirograph_.AddNode({150, 0, 0}, 20.f, true);
-    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
-    layers_.push_back(layer);
-
-    layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
-      ofDrawLine(layer.previous_brush_position_,
-                 layer.spirograph_.brush_position());
-    };
-  }
+  // Interal layer of the center flower 4
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
@@ -186,10 +159,26 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(0);
+      ofSetLineWidth(3);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+
+  // Interal layer of the center flower 3
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({150, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
       ofPushMatrix();
       ofRotateZDeg(360.f / 40.f);
       {
-        ofSetColor(80);
+        ofSetColor(0);
         ofSetLineWidth(2);
         ofDrawLine(layer.previous_brush_position_,
                    layer.spirograph_.brush_position());
@@ -197,6 +186,45 @@ void ofApp::setup() {
       ofPopMatrix();
     };
   }
+
+  // Interal layer of the center flower 2
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({140, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofSetColor(0);
+      ofSetLineWidth(2);
+      ofDrawLine(layer.previous_brush_position_,
+                 layer.spirograph_.brush_position());
+    };
+  }
+
+  // Interal layer of the center flower 1
+  {
+    Layer layer;
+    layer.spirograph_.AddNode({0, 0, 0}, 1.f);
+    layer.spirograph_.AddNode({130, 0, 0}, 20.f, true);
+    layer.spirograph_.AddNode({5, 0, 0}, 0, true);
+    layers_.push_back(layer);
+
+    layers_.back().draw_ = [](const Layer& layer) {
+      ofPushMatrix();
+      ofRotateZDeg(360.f / 40.f);
+      {
+        ofSetColor(0);
+        ofSetLineWidth(2);
+        ofDrawLine(layer.previous_brush_position_,
+                   layer.spirograph_.brush_position());
+      }
+      ofPopMatrix();
+    };
+  }
+
+  // Internal dense spirograph
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.1f);
@@ -205,12 +233,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
-      ofSetLineWidth(2);
+      ofSetColor(0);
+      ofSetLineWidth(1);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Small flower
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
@@ -219,12 +249,14 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
+      ofSetColor(0);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
     };
   }
+
+  // Small flower center
   {
     Layer layer;
     layer.spirograph_.AddNode({0, 0, 0}, 1.f);
@@ -232,7 +264,7 @@ void ofApp::setup() {
     layers_.push_back(layer);
 
     layers_.back().draw_ = [](const Layer& layer) {
-      ofSetColor(80);
+      ofSetColor(0);
       ofSetLineWidth(2);
       ofDrawLine(layer.previous_brush_position_,
                  layer.spirograph_.brush_position());
@@ -251,6 +283,7 @@ void ofApp::update() {
                   layers_.end());
   }
 
+  // Rotate the spirograph brushes
   for (auto& layer : layers_) {
     layer.previous_brush_position_ = layer.spirograph_.brush_position();
     layer.spirograph_.nodes_mutable()[0]->RotateZ();
@@ -263,7 +296,7 @@ void ofApp::draw() {
   ofPushMatrix();
   ofTranslate(ofGetWidth() / 2, ofGetHeight() / 2);
   {
-    for (auto& layer : layers_) {
+    for (const auto& layer : layers_) {
       layer.draw();
     }
   }
