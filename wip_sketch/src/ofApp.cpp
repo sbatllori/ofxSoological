f10e42eca991fd803d5a59c2ae5dcba822ee8f71
Sonia Batllori <sbatllori@hotmail.com>
2021-01-30 14:12:10 +0100

    add(create ordered independent sinus representations)

--- wip_sketch/src/ofApp.cpp
+++ wip_sketch/src/ofApp.cpp
@@ -1,4 +1,5 @@
 #include "ofApp.h"
+#include "soo_color_palettes.h"
 #include "soo_export.h"
 
 namespace {
@@ -13,29 +14,25 @@ ofVec2f f_sin(const float t, const float wave_frequency,
 
   return ofVec2f{x, y};
 }
-}  // namespace
 
-ofColor ofApp::GetBlendedColor(const float t, const int palette_loops) {
-  const int size = color_palette_.size();
-
+ofColor GetBlendedColor(const float t, const soo::colors::Palette& palette,
+                        const float margin = 0.f,
+                        const float palette_loops = 1.f) {
   // Get the continuous color index corresponding to the given value
   // - Use its integral part to determine the current base color of the palette
-  // - Use its fractional part to determine the amount of blending between the
-  // current base color of the palette and the next one
-  const float idx = ofMap(t, -kRadius_, ofGetWidth() + kRadius_, 0,
-                          palette_loops * (size - 1));
-
+  // - Use its fractional part to determine the amount of blending
+  const float idx = ofMap(t, margin, ofGetWidth() - margin, 0,
+                          palette_loops * (palette.size() - 1));
   float integral_f;
   const float fractional = modf(idx, &integral_f);
   const int integral = integral_f;
 
-  // Get the color corresponding to the current position
+  // Get the colors corresponding to the current and the next position
   ofColor color;
-  color.setHex(color_palette_[integral % size]);
+  color.setHex(palette[integral % palette.size()]);
 
-  // Get the color corresponding to the next position
   ofColor next_color;
-  next_color.setHex(color_palette_[(integral + 1) % size]);
+  next_color.setHex(palette[(integral + 1) % palette.size()]);
 
   // Mix the colors with the corresponding amount
   color.lerp(next_color, fractional);
@@ -43,70 +40,128 @@ ofColor ofApp::GetBlendedColor(const float t, const int palette_loops) {
   return color;
 }
 
+}  // namespace
+
 //--------------------------------------------------------------
 void ofApp::setup() {
   // Setup canvas
-  bg_color_.setHex(BG);
-  ofSetFrameRate(30);
+  ofBackground(0);
+  ofSetFrameRate(60);
   ofSetCircleResolution(72);
 
   // Define random values along the canvas width
-  values_.reserve(kNumValues_);
-  std::generate_n(std::back_inserter(values_), kNumValues_, [this]() {
-    return static_cast<int>(ofRandom(-kRadius_, ofGetWidth() + kRadius_));
-  });
-
-  // Temp gui
-  gui_.setup();
-  gui_.add(background_color_slider.setup("background", bg_color_, ofColor(0),
-                                         ofColor(255)));
+  for (int i{kHMargin_}; i < ofGetWidth() - kHMargin_; i++) {
+    values_.push_back(i);
+    init_values_.push_back(i);
+  }
 }
 
 //--------------------------------------------------------------
 void ofApp::update() {
-  //  if (0 < ofGetFrameNum() && ofGetFrameNum() <= 200)
-  //    soo::SaveFrame(ofGetFrameNum());
+  if (0 < ofGetFrameNum() && ofGetFrameNum() <= 200) {
+    //    soo::SaveFrame(ofGetFrameNum());
+  }
 
-  // Increase the values in a loop inside the canvas
+  // Make the values loop inside values range
   for (auto& t : values_) {
     t++;
-    t = t == 0 ? -kRadius_ : t;
-    t = t % (ofGetWidth() + kRadius_);
+    t = t > ofGetWidth() - kHMargin_ ? kHMargin_ : t;
   }
 }
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-  ofBackground(background_color_slider);
-
+  ofVec2f p;
+  ofColor color;
   ofFill();
 
-  for (const auto& t : values_) {
-    for (int i{0}; i < 9; i++) {
-      // Function to be represented
-      const ofVec2f p = f_sin(t, -0.5f * (i + 1), 50.f);
-      const float x = p.x;
-      const float y = p.y + 150 + i * 90;
+  // background composition: soft thin lines building a net
+  for (int i{0}; i < values_.size(); i++) {
+    const auto& t = values_[i];
+    const auto& t0 = init_values_[i];
 
-      // Color
-      const ofColor color = GetBlendedColor(t, i % 4 + 2);
+    // ===>
+    for (int i{0}; i < 50; i++) {
+      p = f_sin(t0, 1.5f - .1f * i, 150.f);
+      color = GetBlendedColor(-t, soo::colors::blues_of_the_sea, kHMargin_,
+                              i * 0.1 + 1);
 
-      // Draw
-      ofSetColor(color.r, color.g, color.b, 255);
-      ofDrawCircle(x, y, .75f * kRadius_);
-
-      ofSetColor(color.r, color.g, color.b, 150);
-      ofDrawCircle(x, y, kRadius_);
-
-      // Draw transparency
-      ofSetColor(color.r, color.g, color.b, 5);
-      ofDrawCircle(x, y, 1.25f * kRadius_);
-      ofDrawCircle(x, y, 1.5f * kRadius_);
-      ofDrawCircle(x, y, 2.f * kRadius_);
+      ofSetColor(color.r, color.g, color.b, 20);
+      ofDrawCircle(p.x, p.y - 150 + 20 * i, 5);
     }
+    // <===
   }
 
-  //  gui_.draw();
+  // foreground composition: thiker lines
+  for (int i{0}; i < values_.size(); i++) {
+    const auto& t = values_[i];
+    const auto& t0 = init_values_[i];
+
+    color = GetBlendedColor(-t, soo::colors::blues_of_the_sea, kHMargin_, 5.f);
+    ofSetColor(color.r, color.g, color.b, 70);
+
+    // ===>
+    p = f_sin(t0, .5f, 50.f);
+    ofDrawCircle(p.x, p.y + 50, 10);
+    // <===
+
+    // ===>
+    p = f_sin(t0, .5f, 10.f);
+    ofDrawCircle(p.x, p.y + 85, 10);
+    // <===
+
+    // ===>
+    p = f_sin(t0, .5f, 100.f);
+    ofDrawCircle(p.x, p.y + 40, 7);
+    // <===
+
+    // ===>
+    p = f_sin(t0, .5f, 130.f);
+    ofDrawCircle(p.x, p.y + 30, 5);
+    // <===
+
+    // ===>
+    p = f_sin(t0, .5f, 250.f);
+    ofDrawCircle(p.x, p.y + 100, 100);
+    // <===
+
+    // ===>
+    for (int i{0}; i < 8; i++) {
+      p = f_sin(t0, 1.5f - .2f * i, 150.f - 10.f * i);
+      ofDrawCircle(p.x, p.y + 350 + 60 * i, 20 - 2 * i);
+    }
+    // <===
+
+    // ===>
+    for (int i{0}; i < 3; i++) {
+      const float sign = i % 2 == 0 ? 1.f : -1.f;
+      p = f_sin(t0, sign * 3.5f, 30.f + 20.f * i);
+      ofDrawCircle(p.x, p.y + 850 + 15 * i, 6 - 2 * i);
+    }
+    // <===
+
+    // ===>
+    p = f_sin(t0, 1.f, 100.f);
+    ofDrawCircle(p.x, ofGetHeight() - p.y, 80);
+    // <===
+  }
+
+  //  for (int i{0}; i < values_.size(); i++) {
+  //    const auto& t = values_[i];
+  //    const auto& t0 = init_values_[i];
+
+  //    color =
+  //        GetBlendedColor(t, soo::colors::shiny_black_gradient,
+  //        kHMargin_, 5.f);
+  //    ofSetColor(color.r, color.g, color.b, 10);
+
+  //    // ===>
+  //    for (int i{0}; i < 12; i++) {
+  //      p = f_sin(t0, i * 1.5f, 40.f * sin((12 - i) * ofGetElapsedTimef()));
+  //      ofDrawCircle(p.x, p.y + 40 + i * 100, 2);
+  //    }
+  //    // <===
+  //  }
 }
 
 //--------------------------------------------------------------
--- wip_sketch/src/ofApp.h
+++ wip_sketch/src/ofApp.h
@@ -1,16 +1,7 @@
 #pragma once
 
-#define BLUE_BG 0xedd3b2
-#define PINK_BG 0x93cea8
-#define WHITE_BG 0x181818
-#define BLACK_BG 0xffffff
-#define YELLOW_BG 0xda96ff
-
-#define BG WHITE_BG
-
 #include "ofMain.h"
 #include "ofxGui.h"
-#include "soo_color_palettes.h"
 #include "soo_noise.h"
 
 class ofApp : public ofBaseApp {
@@ -21,18 +12,7 @@ class ofApp : public ofBaseApp {
   void keyPressed(int key);
 
  private:
-  // GUI
-  ofxPanel gui_;
-  ofxColorSlider background_color_slider;
-
-  ofColor bg_color_;
-  const soo::colors::Palette color_palette_{
-      soo::colors::warm_yellow_monochromatic};
-
-  const int kRadius_{20};
-  const int kNumValues_{700};
+  const int kHMargin_{-100};
   std::vector<int> values_;
-
- private:
-  ofColor GetBlendedColor(const float t, const int palette_loops = 1);
+  std::vector<int> init_values_;
 };
